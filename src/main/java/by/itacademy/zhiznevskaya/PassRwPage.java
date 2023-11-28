544a4dfa863bc00a7d1656d8c2dc80e2f7652529
zvu <zvu@agsr.by>
2023-11-28 15:35:35 +0300

    add PassRwPage and PassRwTest

--- src/main/java/by/itacademy/zhiznevskaya/PassRwPage.java
+++ src/main/java/by/itacademy/zhiznevskaya/PassRwPage.java
@@ -5,8 +5,18 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class PassRwPage {
-    public String personalCabinetXpath="//*[contains(text(),\'Личный кабинет\')]'";
+    public String personalCabinetXpath="//span[contains(text(),'Личный кабинет')]";
     public String modelDialogCheckboxTextXpath="//span[text()='Больше не показывать']";
+    public String formAuthorizationXpath="//*[contains(text(),'Авторизация')]";
+    public String authorizationFormLoginXpath ="//input[contains(@name,'login')]";
+    public String authorizationFormPasswordXpath ="//input[contains(@name,'password')]";
+
+    public String authorizationFormEnterButtonXpath="//input[contains(@value,'Войти')]";
+    public String authorizationFormErrorEmptyFieldXpath="//label[contains(text(),'Заполните поле')]";
+
+    public String authorizationFormErrorEmptyLoginFieldXpath="//label[contains(@for,'login')]";
+
+    public String authorizationFormErrorUserNotFoundXpath="//div[contains(text(),'Пользователь не найден')]";
     private WebDriver driver;
 
     public PassRwPage(WebDriver driver) {
@@ -25,4 +35,57 @@ public class PassRwPage {
         modelDialogCheckBoxElement.click();
     }
 
+    public boolean isAuthorizationFormOpened(){
+        boolean isFormOpened=false;
+        By authorizationFormOpenedBy=By.xpath(formAuthorizationXpath);
+        WebElement formAuthorizationXpathElement=driver.findElement(authorizationFormOpenedBy);
+
+        if(formAuthorizationXpathElement.isDisplayed()){
+            isFormOpened=true;
+        }
+        else
+        {
+            isFormOpened=false;
+        }
+    return isFormOpened;
+    }
+
+    public String enterEmptyFieldsAuthorizationForm(){
+        By authorizationFormLoginBy=By.xpath(authorizationFormLoginXpath);
+        WebElement authorizationFormLoginElement= driver.findElement(authorizationFormLoginBy);
+        authorizationFormLoginElement.sendKeys("");
+        By authorizationFormPasswordBy=By.xpath(authorizationFormPasswordXpath);
+        WebElement authorizationFormPasswordElement=driver.findElement(authorizationFormPasswordBy);
+        authorizationFormPasswordElement.sendKeys("");
+        By authorizationFormEnterButtonBy=By.xpath(authorizationFormEnterButtonXpath);
+        WebElement authorizationFormEnterButtonElement=driver.findElement(authorizationFormEnterButtonBy);
+        authorizationFormEnterButtonElement.click();
+        return driver.findElement(By.xpath(authorizationFormErrorEmptyFieldXpath)).getText();
+    }
+
+    public String enterLoginEmptyFieldsAuthorizationForm(){
+        By authorizationFormLoginBy=By.xpath(authorizationFormLoginXpath);
+        WebElement authorizationFormLoginElement= driver.findElement(authorizationFormLoginBy);
+        authorizationFormLoginElement.sendKeys("");
+        By authorizationFormPasswordBy=By.xpath(authorizationFormPasswordXpath);
+        WebElement authorizationFormPasswordElement=driver.findElement(authorizationFormPasswordBy);
+        authorizationFormPasswordElement.sendKeys("123456");
+        By authorizationFormEnterButtonBy=By.xpath(authorizationFormEnterButtonXpath);
+        WebElement authorizationFormEnterButtonElement=driver.findElement(authorizationFormEnterButtonBy);
+        authorizationFormEnterButtonElement.click();
+        return driver.findElement(By.xpath(authorizationFormErrorEmptyLoginFieldXpath)).getText();
+    }
+
+    public String enterNotFoundUserAuthorizationForm(){
+        By authorizationFormLoginBy=By.xpath(authorizationFormLoginXpath);
+        WebElement authorizationFormLoginElement= driver.findElement(authorizationFormLoginBy);
+        authorizationFormLoginElement.sendKeys("email@gmail.com");
+        By authorizationFormPasswordBy=By.xpath(authorizationFormPasswordXpath);
+        WebElement authorizationFormPasswordElement=driver.findElement(authorizationFormPasswordBy);
+        authorizationFormPasswordElement.sendKeys("123456");
+        By authorizationFormEnterButtonBy=By.xpath(authorizationFormEnterButtonXpath);
+        WebElement authorizationFormEnterButtonElement=driver.findElement(authorizationFormEnterButtonBy);
+        authorizationFormEnterButtonElement.click();
+        return driver.findElement(By.xpath(authorizationFormErrorUserNotFoundXpath)).getText();
+    }
 }
--- src/test/java/by/itacademy/zhiznevskaya/PassRwTest.java
+++ src/test/java/by/itacademy/zhiznevskaya/PassRwTest.java
@@ -1,10 +1,15 @@
 package by.itacademy.zhiznevskaya;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class PassRwTest extends BaseTest{
+import java.util.Iterator;
+import java.util.Set;
+
+public class PassRwTest extends BaseTest {
     @Test
     public void testPassRw() throws InterruptedException {
         driver.get("https://pass.rw.by/ru/");
@@ -13,4 +18,86 @@ public class PassRwTest extends BaseTest{
         Thread.sleep(5000);
         passRwPagePersonalCabinet.clickLinkPersonalCabinet();
     }
+
+    @Test
+    public void testAuthorizationFormOpen() throws InterruptedException {
+        driver.get("https://pass.rw.by/ru/");
+        Thread.sleep(5000);
+        PassRwPage passRwPagePersonalCabinet = new PassRwPage(driver);
+        passRwPagePersonalCabinet.clickLinkPersonalCabinet();
+        Thread.sleep(5000);
+        String mainWindowHandle = driver.getWindowHandle();
+        Set<String> windowHandles = driver.getWindowHandles();
+        Iterator<String> itr = windowHandles.iterator();
+        while (itr.hasNext()) {
+            String childWindowHandle = itr.next();
+            // to skip the handle of our main window and switch to new one
+            if (!mainWindowHandle.equalsIgnoreCase(childWindowHandle))
+                driver.switchTo().window(childWindowHandle);
+        }
+        boolean actual = passRwPagePersonalCabinet.isAuthorizationFormOpened();
+        boolean expected = true;
+        Assertions.assertEquals(expected, actual);
+
+}
+    @Test void testAuthorizationFormEmptyFieldsInput() throws InterruptedException {
+        driver.get("https://pass.rw.by/ru/");
+        Thread.sleep(5000);
+        PassRwPage passRwPagePersonalCabinet = new PassRwPage(driver);
+        passRwPagePersonalCabinet.clickLinkPersonalCabinet();
+        Thread.sleep(5000);
+        String mainWindowHandle = driver.getWindowHandle();
+        Set<String> windowHandles = driver.getWindowHandles();
+        Iterator<String> itr = windowHandles.iterator();
+        while (itr.hasNext()) {
+            String childWindowHandle = itr.next();
+            // to skip the handle of our main window and switch to new one
+            if (!mainWindowHandle.equalsIgnoreCase(childWindowHandle))
+                driver.switchTo().window(childWindowHandle);
+        }
+        String actual = passRwPagePersonalCabinet.enterEmptyFieldsAuthorizationForm();
+        String expected = "Заполните поле";
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test void testAuthorizationFormLoginEmptyFieldInput() throws InterruptedException {
+        driver.get("https://pass.rw.by/ru/");
+        Thread.sleep(5000);
+        PassRwPage passRwPagePersonalCabinet = new PassRwPage(driver);
+        passRwPagePersonalCabinet.clickLinkPersonalCabinet();
+        Thread.sleep(5000);
+        String mainWindowHandle = driver.getWindowHandle();
+        Set<String> windowHandles = driver.getWindowHandles();
+        Iterator<String> itr = windowHandles.iterator();
+        while (itr.hasNext()) {
+            String childWindowHandle = itr.next();
+            // to skip the handle of our main window and switch to new one
+            if (!mainWindowHandle.equalsIgnoreCase(childWindowHandle))
+                driver.switchTo().window(childWindowHandle);
+        }
+        String actual = passRwPagePersonalCabinet.enterLoginEmptyFieldsAuthorizationForm();
+        String expected = "Заполните поле";
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test void testAuthorizationFormUserNotFoundInput() throws InterruptedException {
+        driver.get("https://pass.rw.by/ru/");
+        Thread.sleep(5000);
+        PassRwPage passRwPagePersonalCabinet = new PassRwPage(driver);
+        passRwPagePersonalCabinet.clickLinkPersonalCabinet();
+        Thread.sleep(5000);
+        String mainWindowHandle = driver.getWindowHandle();
+        Set<String> windowHandles = driver.getWindowHandles();
+        Iterator<String> itr = windowHandles.iterator();
+        while (itr.hasNext()) {
+            String childWindowHandle = itr.next();
+            // to skip the handle of our main window and switch to new one
+            if (!mainWindowHandle.equalsIgnoreCase(childWindowHandle))
+                driver.switchTo().window(childWindowHandle);
+
+        }
+            String actual =passRwPagePersonalCabinet.enterNotFoundUserAuthorizationForm();
+            Thread.sleep(5000);
+            Assertions.assertEquals("Пользователь не найден",actual);
+        }
 }
